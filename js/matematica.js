d2dcb82cecf2cc2987dd4cc84221903a81972f59
GeraldRC <gerald.reyes@outlook.cl>
2020-06-16 21:25:46 -0400

    avanze 3
    renovacion de interfaces , lenguaje, matemativa y ciencias

---
+++ js/matematica.js
@@ -0,0 +1,10 @@
+// cerrar Sesion
+var btnS= document.getElementById('cerrarSession');
+
+btnS.addEventListener('click',function(){
+    setTimeout(function(){
+        location.href ="login.html";
+    }, 1000);
+});
+
+// seguir aca
